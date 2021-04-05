c72d782293be1b83d70c79efea699073dd1b8bf8
Mario Hartleb <mhartleb83@yahoo.com>
2021-04-05 18:15:16 +0200

    check 05.04.21

--- script/myscript.js
+++ script/myscript.js
@@ -8,6 +8,10 @@ console.table(movieData);
 // function sortMovies() {
 //   movieData.sort((a, b) => b.likes - a.likes);}
 
+
+
+
+
 for (let i in movieData) {
     
     
@@ -24,8 +28,8 @@ for (let i in movieData) {
                         <h5 class="card-title">${movieData[i].movieName}</h5>
                         <p class="card-text">${movieData[i].description}</p>
                         <div class="card-footer">
-                            <button${i} class="btn btn-success click-button">Like &#128077</button>
-                            <span${i} class="dotResult">0</span>
+                            <button class="btn btn-success" id="click-button">Like &#128077</button>
+                            <span id="dotResult">${movieData[i].likes}</span>
                         </div>
                     </div>
                 </div>
@@ -33,6 +37,12 @@ for (let i in movieData) {
         </div>
     </div>
     `
+} 
+// end for-in loop
+
+
+
+
 
 /* I had two approaches, unfortuntely the second one (commented out) didn't work eventhough the index was added to the id (see consolelike 
 Line 22: id="like-btn${[i]}", same for Line23 id="dotResult${[i]}".
@@ -41,31 +51,40 @@ The other approach just works for the first card.
 
 
 
-//     var button = document.getElementById("like-btn"),
-//         count = 0;
-//         button.onclick = function() {
-//         count += 1;
-//     document.getElementById("dotResult").innerHTML = "Likes: " + count;
-//   };
-  
-// }
-    const clickBtn = document.querySelectorAll("button");
+
+
+
+
+
+// const clickBtn = document.querySelectorAll("#clickbutton");
     
-    for (let j=0; j < clickBtn.length; j++){        
-        function clickLike () {
-            let inputNumber = document.getElementsByClassName("dotResult")[j];
-            let dataCounter = (number(movieData[j].likes) += 1);
-            console.log(dataCounter)
-            inputNumber.innerHTML = dataCounter;
-        };
-    }
-    clickBtn.addEventListener("click", clickLike);
+// for (let i=0; i < clickBtn.length; i++){        
+//     function clickLike () {
+//         let inputNumber = document.getElementsByClassName("dotResult")[i];
+//         let dataCounter = (number(movieData[i].likes) += 1);
+//         console.log(dataCounter)
+//         inputNumber.innerHTML = dataCounter;
+//     };
+// }
+// clickBtn.addEventListener("click", clickLike);
 
-console.log(clickBtn);
+// console.log(clickBtn);
 
-}
-
-
+// let likeBtn = document.querySelectorAll("#click-button");
+// ​
+// likeBtn.forEach(function(currentBtn){
+// ​
+//     currentBtn.addEventListener("click", function(){
+// ​
+//         let like = this.parentNode.querySelector("#dotResult").innerHTML;
+// ​
+//         like = parseInt(like);
+// ​
+//         this.parentNode.querySelector("#dotResult").innerHTML = like + 1;
+// ​
+//     })
+// ​
+// })
 
 
 
