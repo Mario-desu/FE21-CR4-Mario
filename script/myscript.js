8bcfeeb67ef51d4e56dfbc7ae464c25470b6efef
Mario Hartleb <mhartleb83@yahoo.com>
2021-05-20 00:30:34 +0200

    fix Click-Counter and Sorting

--- script/myscript.js
+++ script/myscript.js
@@ -10,93 +10,87 @@ console.table(movieData);
 
 
 
+function displayCards(){
 
-
-for (let i in movieData) {
+for (i = 0; i < movieData.length; i++) {
     
     
 
     document.getElementById("card-wrapper").innerHTML += `
     <div class="col">
-        <div class="card">
+        <div class="card shadow rounded">
             <div class="row g-0">
                 <div class="col-md-4">
-                <img src="${movieData[i].image}" class="img-fluid" alt="Movie poster">
+                <img src="${movieData[i].image}" class="img-fluid rounded" alt="Movie poster">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
                         <h5 class="card-title">${movieData[i].movieName}</h5>
                         <p class="card-text">${movieData[i].description}</p>
-                        <div class="card-footer">
-                            <button class="btn btn-success" id="click-button">Like &#128077</button>
-                            <span id="dotResult">${movieData[i].likes}</span>
-                        </div>
                     </div>
+                    <div class="card-footer likeBtn" id="randCard${[i]}">
+                    
+                    <btn class="likeCount btn btn-primary rounded-circle disabled">${movieData[i].likes} *</btn><br><br>
+                </div>
                 </div>
             </div>
         </div>
     </div>
     `
 } 
-// end for-in loop
+// end for-loop
+
+}//end function displayCards()
+
+displayCards();
 
 
+//Click-Counter
 
+function btnLikes() {
+    for (let i in movieData) {
+      let buttonCount = document.getElementsByClassName("likeBtn");
+      let countTarget = document.getElementsByClassName("likeCount");
+      buttonCount[i].innerHTML +=
+        "<button class='btn btn-success blike'>Like &#128077</button>";
+      let bcount = document.getElementsByClassName("blike");
+      bcount[i].addEventListener("click", addLike);
+      function addLike() {
+        let up = parseInt(movieData[i].likes) + 1;
+        countTarget[i].innerHTML = up + " *";
+        console.log(up);
+        movieData[i].likes = up;
 
-
-/* I had two approaches, unfortuntely the second one (commented out) didn't work eventhough the index was added to the id (see consolelike 
-Line 22: id="like-btn${[i]}", same for Line23 id="dotResult${[i]}".
-The other approach just works for the first card.
-*/
-
-
-
-
-
-
-
-
-// const clickBtn = document.querySelectorAll("#clickbutton");
+      }
+    }
     
-// for (let i=0; i < clickBtn.length; i++){        
-//     function clickLike () {
-//         let inputNumber = document.getElementsByClassName("dotResult")[i];
-//         let dataCounter = (number(movieData[i].likes) += 1);
-//         console.log(dataCounter)
-//         inputNumber.innerHTML = dataCounter;
-//     };
-// }
-// clickBtn.addEventListener("click", clickLike);
+  }
+  
+  btnLikes();
 
-// console.log(clickBtn);
 
-// let likeBtn = document.querySelectorAll("#click-button");
-// ​
-// likeBtn.forEach(function(currentBtn){
-// ​
-//     currentBtn.addEventListener("click", function(){
-// ​
-//         let like = this.parentNode.querySelector("#dotResult").innerHTML;
-// ​
-//         like = parseInt(like);
-// ​
-//         this.parentNode.querySelector("#dotResult").innerHTML = like + 1;
-// ​
-//     })
-// ​
-// })
+//Sort-Button
+
+
+
+document.getElementById("movieSort").addEventListener("click", sortBtn);
+
+// Clear card-wrapper from old cards
+function sortBtn() {
+    movieData.sort((a, b) => b.likes - a.likes);
+    const newCards = document.getElementById("card-wrapper");
+    newCards.textContent = "";
+    displayCards();
+    btnLikes();
+  }
+  
 
 
 
 
 
- 
 
 
-
-
-
- 
 
 
 
